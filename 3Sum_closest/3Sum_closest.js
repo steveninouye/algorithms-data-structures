083c3741930660c54714a3fdcc8bab71999f28c5
Steven Inouye <steveninouye@msn.com>
2019-02-08 16:13:40 -0800

    First attempt to closest sum

--- 3Sum_closest/3Sum_closest.js
+++ 3Sum_closest/3Sum_closest.js
@@ -10,5 +10,33 @@ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 */
 
 const threeSumClosest = (nums, target) => {
-  
+  nums.sort((a, b) => a - b);
+  let closestSum = Infinity;
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i !== 0 && nums[i] === nums[i - 1]) continue;
+    let num = nums[i];
+    let lo = i + 1;
+    let hi = nums.length - 1;
+    while (lo < hi) {
+      let sum = num + nums[lo] + nums[hi];
+      if (sum === target) {
+        return target;
+      }
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      if (sum < target) {
+        lo++;
+        while (nums[lo] === nums[lo - 1]) {
+          lo++;
+        }
+      } else {
+        hi--;
+        while (nums[hi] === nums[hi + 1]) {
+          hi--;
+        }
+      }
+    }
+  }
+  return closestSum;
 };
