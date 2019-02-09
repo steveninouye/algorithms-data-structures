5f92a8b19ff4632321c0742bd1aaa1703a80fecb
Steven Inouye <steveninouye@msn.com>
2019-02-09 14:13:12 -0800

    Change directory name to lru cache

---
+++ lru_cache/lib/p04_linked_list.rb
@@ -0,0 +1,94 @@
+require "byebug"
+
+class Node
+  attr_reader :key
+  attr_accessor :val, :next, :prev
+
+  def initialize(key = nil, val = nil)
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
+  end
+
+  def to_s
+    "#{@key}: #{@val}"
+  end
+
+  def remove
+    # optional but useful, connects previous link to next link
+    # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
+    @next = nil
+    @prev = nil
+  end
+end
+
+class LinkedList
+  include Enumerable
+
+  def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
+  end
+
+  def [](i)
+    each_with_index { |link, j| return link if i == j }
+    nil
+  end
+
+  def first
+    @head.next
+  end
+
+  def last
+    @tail.prev
+  end
+
+  def empty?
+    @head.next == @tail && @tail.prev == @head
+  end
+
+  def get(key)
+    each { |node| return node.val if node.key == key }
+    nil
+  end
+
+  def include?(key)
+    each { |node| return true if node.key == key }
+    false
+  end
+
+  def append(key, val)
+    node = Node.new(key, val)
+    node.prev = @tail.prev
+    node.prev.next = node
+    @tail.prev = node
+    node.next = @tail
+  end
+
+  def update(key, val)
+    each { |node| node.val = val if node.key == key }
+  end
+
+  def remove(key)
+    each { |node| node.remove if node.key == key }
+  end
+
+  def each(&prc)
+    node = first
+    while node != @tail
+      next_node = node.next
+      prc.call(node)
+      node = next_node
+    end
+  end
+
+  # uncomment when you have `each` working and `Enumerable` included
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
+end
