31a5c5485e75238648157d84b6475afba953480e
Steven Inouye <steveninouye@msn.com>
2019-02-08 16:33:53 -0800

    First attemp to letter combinations of a phone nubmer

--- letter_combinations_of_a_phone_number/letter_combinations_of_a_phone_number.js
+++ letter_combinations_of_a_phone_number/letter_combinations_of_a_phone_number.js
@@ -14,4 +14,26 @@ Note:
 
 Although the above answer is in lexicographical order, your answer could be in any order you want.
 */
-const letterCombinations = (digits) => {};
+const letterCombinations = (digits) => {
+  if (digits.length === 0) return [];
+  const map = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z']
+  };
+  if (digits.length === 1) return map[digits];
+  const result = [];
+  let el = digits[0];
+  let prev = letterCombinations(digits.slice(1));
+  for (let str of prev) {
+    for (let ltr of map[el]) {
+      result.push(ltr + str);
+    }
+  }
+  return result;
+};
