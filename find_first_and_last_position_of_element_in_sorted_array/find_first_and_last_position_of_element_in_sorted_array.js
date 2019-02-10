6e9f83d067b45ff3b770d84d98848ce688f0c1dd
Steven Inouye <steveninouye@msn.com>
2019-02-09 20:19:47 -0800

    Refactor find first and last position of element in sorted array

--- find_first_and_last_position_of_element_in_sorted_array/find_first_and_last_position_of_element_in_sorted_array.js
+++ find_first_and_last_position_of_element_in_sorted_array/find_first_and_last_position_of_element_in_sorted_array.js
@@ -16,46 +16,45 @@ Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
 */
 
-const searchRange = (nums, target, side = null /* R or L */) => {
-  if (nums.length === 0 || (nums.length === 1 && nums[0] !== target))
-    return [-1, -1];
-  // length of nums is less than or equal to 1
-  ////// if equal to target return 0
-  ////// if not equal return -1
-  let midIdx = Math.floor((nums.length - 1) / 2);
-  let midEl = nums[midIdx];
-  // find middle index
-  // find element at middle index
-  if (midEl === target) {
-    let left = searchRange(nums.slice(0, midIdx), target)[0];
-    left = left === -1 ? midIdx : left;
-    let right = searchRange(nums.slice(midIdx + 1), target)[1];
-    right = right === -1 ? midIdx : midIdx + 1 + right;
-    return [left, right];
-  } else if (midEl < target) {
-    // right
-    let right = searchRange(nums.slice(midIdx + 1), target);
-    if (right[0] === -1) {
-      return [-1, -1];
-    } else {
-      return [right[0] + 1 + midIdx, right[1] + 1 + midIdx];
+const searchRange = (nums, target) => {
+  let left = (() => {
+    let start = 0;
+    let end = nums.length - 1;
+    let result;
+    while (end - start >= 0) {
+      let midIdx = start + Math.floor((end - start) / 2);
+      let midEl = nums[midIdx];
+      if (midEl === target) {
+        result = midIdx;
+        end = midIdx - 1;
+      } else if (midEl < target) {
+        start = midIdx + 1;
+      } else {
+        end = midIdx - 1;
+      }
     }
-  } else {
-    // left
-    let left = searchRange(nums.slice(0, midIdx), target);
-    if (left[0] === -1) {
-      return [-1, -1];
-    } else {
-      return left;
+    return result;
+  })();
+  if (left === undefined) return [-1, -1];
+  let right = (() => {
+    let start = left;
+    let end = nums.length - 1;
+    let result;
+    while (end - start >= 0) {
+      let midIdx = start + Math.floor((end - start) / 2);
+      let midEl = nums[midIdx];
+      if (midEl === target) {
+        result = midIdx;
+        start = midIdx + 1;
+      } else if (midEl < target) {
+        start = midIdx + 1;
+      } else {
+        end = midIdx - 1;
+      }
     }
-  }
-  // if middle element equals target
-  //////// find left bound
-  ///////////// cut left in half
-  ///////////////// if equals to element
-  ///////////////// check left bound again
-  ///////////////// I know when found left bound when middle element equals target && left bound return -1
-  //////// find right bound
+    return result;
+  })();
+  return [left, right];
 };
 
 console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
