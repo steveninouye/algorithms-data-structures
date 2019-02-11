27b8f9b054459716111b4a01f056c22fe61aaae8
Steven Inouye <steveninouye@msn.com>
2019-02-11 14:29:48 -0800

    Write test for rotate image

--- rotate_image/rotate_image.js
+++ rotate_image/rotate_image.js
@@ -43,3 +43,25 @@ rotate the input matrix in-place such that it becomes:
 */
 
 const rotate = (matrix) => {};
+
+console.log(
+  'output=>',
+  rotate([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]])
+);
+
+let result = [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]];
+
+console.log(
+  rotate(
+    [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]].reduce(
+      (acc, row, i) => {
+        let ansRow = result[i];
+        for (let j = 0; j < row.length; j++) {
+          if (row[j] !== ansRow[j]) acc = false;
+        }
+        return acc;
+      },
+      true
+    )
+  )
+);
