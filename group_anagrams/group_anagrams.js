aad9c0da601c9dd8b40a59f5a158ce437b13549a
Steven Inouye <steveninouye@msn.com>
2019-02-12 11:44:16 -0800

    Working out but with group anagrams

--- group_anagrams/group_anagrams.js
+++ group_anagrams/group_anagrams.js
@@ -17,4 +17,60 @@ All inputs will be in lowercase.
 The order of your output does not matter.
 */
 
-const groupAnagrams = (strs) => {};
+const groupAnagrams = (strs) => {
+  if (strs.length === 0) return [];
+  let words = [];
+  let result = {};
+  let word = new Word(strs[0]);
+  words.push(word);
+  result[strs[0]] = [strs[0]];
+  for (let i = 1; i < strs.length; i++) {
+    let str = strs[i];
+    let word = new Word(strs[i]);
+    let len = words.length;
+    for (let j = 0; j < len; j++) {
+      if (words[j].isAnagram(word)) {
+        result[words[j].word].push(str);
+      } else {
+        words.push(word);
+        result[str] = [str];
+      }
+    }
+  }
+  return Object.values(result);
+};
+
+class Word {
+  constructor(word) {
+    this.word = word;
+    this.letters = {};
+    this.getLetters();
+  }
+
+  getLetters() {
+    let { word } = this;
+    for (let i = 0; i < word.length; i++) {
+      let ltr = word[i];
+      if (!this.letters[ltr]) {
+        this.letters[ltr] = 0;
+      }
+      this.letters[ltr]++;
+    }
+  }
+
+  isAnagram(word) {
+    let keys1 = Object.keys(word.letters);
+    let keys2 = Object.keys(this.letters);
+    if (keys1.length !== keys2.length) return false;
+    for (let key of keys1) {
+      console.log(key);
+      if (this.letters[key] !== word.letters[key]) return false;
+    }
+    return true;
+  }
+}
+
+console.log(
+  'output =>',
+  groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
+);
