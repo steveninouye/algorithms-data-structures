e44b44709ea61b23a986b96e90224e7d6b159fa3
Steven Inouye <steveninouye@msn.com>
2019-02-12 15:36:54 -0800

    Refactor group anagrams

--- group_anagrams/group_anagrams.js
+++ group_anagrams/group_anagrams.js
@@ -18,59 +18,16 @@ The order of your output does not matter.
 */
 
 const groupAnagrams = (strs) => {
-  if (strs.length === 0) return [];
-  let words = [];
-  let result = {};
-  let word = new Word(strs[0]);
-  words.push(word);
-  result[strs[0]] = [strs[0]];
-  for (let i = 1; i < strs.length; i++) {
-    let str = strs[i];
-    let word = new Word(strs[i]);
-    let len = words.length;
-    let isAnagram = false;
-    for (let j = 0; j < len; j++) {
-      if (words[j].isAnagram(word)) {
-        isAnagram = true;
-        result[words[j].word].push(str);
-      }
+  let store = {};
+  for (let str of strs) {
+    let sortedStr = [...str].sort().join('');
+    if (!store[sortedStr]) {
+      store[sortedStr] = [];
     }
-    if (!isAnagram) {
-      words.push(word);
-      result[str] = [str];
-    }
+    store[sortedStr].push(str);
   }
-  return Object.values(result);
+  return Object.values(store);
 };
-
-class Word {
-  constructor(word) {
-    this.word = word;
-    this.letters = {};
-    this.getLetters();
-  }
-
-  getLetters() {
-    let { word } = this;
-    for (let i = 0; i < word.length; i++) {
-      let ltr = word[i];
-      if (!this.letters[ltr]) {
-        this.letters[ltr] = 0;
-      }
-      this.letters[ltr]++;
-    }
-  }
-
-  isAnagram(word) {
-    let keys1 = Object.keys(word.letters);
-    let keys2 = Object.keys(this.letters);
-    if (keys1.length !== keys2.length) return false;
-    for (let key of keys1) {
-      if (this.letters[key] !== word.letters[key]) return false;
-    }
-    return true;
-  }
-}
 
 console.log(
   'output =>',
