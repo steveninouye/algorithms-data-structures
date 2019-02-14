e9ccfbb3d9f573500ce210280b323d9d1fcc342a
Steven Inouye <steveninouye@msn.com>
2019-02-14 14:03:02 -0800

    Pass tests for spiral order

--- spiral_matrix/spiral_matrix.js
+++ spiral_matrix/spiral_matrix.js
@@ -25,18 +25,51 @@ const spiralOrder = (matrix) => {
   // create container variables
   // top = 0, left = 0
   // bottom = matrix.length - 1, right = matrix[0].length - 1
-  let top = 0,
+  let top = 1,
     left = 0,
     bottom = matrix.length - 1,
-    right = matrix.length - 1;
-  let directions = ['r', 'd', 'l', 'u'];
+    right = matrix[0].length - 1;
+  let r = 'r',
+    d = 'd',
+    l = 'l',
+    u = 'u';
+  let directions = [r, d, l, u];
   let dir = 0;
-  let result = [];
   let row = 0,
     col = 0;
-  while (true) {
-    
+  let total = matrix.length * matrix[0].length;
+  let result = [];
+  while (result.length !== total) {
+    let el = matrix[row][col];
+    result.push(el);
+    let direction = directions[dir % 4];
+    if (direction === r) {
+      col++;
+      if (col === right) {
+        dir++;
+        right--;
+      }
+    } else if (direction === 'd') {
+      row++;
+      if (row === bottom) {
+        dir++;
+        bottom--;
+      }
+    } else if (direction === 'l') {
+      col--;
+      if (col === left) {
+        dir++;
+        left++;
+      }
+    } else {
+      row--;
+      if (row === top) {
+        dir++;
+        top++;
+      }
+    }
   }
+  return result;
   // start at top left
   ///// increase col num until hitting right
   ///// decrease right var
