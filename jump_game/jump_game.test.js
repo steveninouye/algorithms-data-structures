47b150c4be6abb7d791e446248a160c4119b35e7
Steven Inouye <steveninouye@msn.com>
2019-02-13 16:16:57 -0800

    pass jump game tests

---
+++ jump_game/jump_game.test.js
@@ -0,0 +1,9 @@
+const canJump = require('./jump_game');
+
+test('[2,3,1,1,4] to be true', () => {
+  expect(canJump([2, 3, 1, 1, 4])).toBe(true);
+});
+
+test('[3,2,1,0,4] to be false', () => {
+  expect(canJump([3, 2, 1, 0, 4])).toBe(false);
+});
--- maximum_subarray/maxiumum_subarray.test.js
+++ maximum_subarray/maxiumum_subarray.test.js
@@ -1,13 +1,13 @@
 const maxSubArray = require('./maximum_subarray');
 
-test('runs', () => {
+test('[-2, 1, -3, 4, -1, 2, 1, -5, 4] to equal 6', () => {
   expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
 });
 
-test('runs', () => {
+test('[-1] to equal -1', () => {
   expect(maxSubArray([-1])).toBe(-1);
 });
 
-test('runs', () => {
+test('[-2, 1] to equal 1', () => {
   expect(maxSubArray([-2, 1])).toBe(1);
 });
