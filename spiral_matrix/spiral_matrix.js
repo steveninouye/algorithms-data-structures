29869edf67bcbf4aee74b0498358f8fd6846d8c6
Steven Inouye <steveninouye@msn.com>
2019-02-13 17:28:03 -0800

    spiral order in progres

--- spiral_matrix/spiral_matrix.js
+++ spiral_matrix/spiral_matrix.js
@@ -22,7 +22,28 @@ Input:
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 */
 const spiralOrder = (matrix) => {
+  // create container variables
+  // top = 0, left = 0
+  // bottom = matrix.length - 1, right = matrix[0].length - 1
+  let top = 0,
+    left = 0,
+    bottom = matrix.length - 1,
+    right = matrix.length - 1;
+  let directions = ['r', 'd', 'l', 'u'];
+  let dir = 0;
+  let result = [];
+  let row = 0,
+    col = 0;
+  while (true) {
     
+  }
+  // start at top left
+  ///// increase col num until hitting right
+  ///// decrease right var
+  ///// increase row num until hitting bottom
+  ///// decrease bottom var
+  ///// decrease col until hitting left
+  ///// increase left
 };
 
-module.exports = spiralOrder
+module.exports = spiralOrder;
