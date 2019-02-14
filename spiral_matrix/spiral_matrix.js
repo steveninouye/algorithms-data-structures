943a7f8f1288ca0588f580882aee9a9620ff9bd3
Steven Inouye <steveninouye@msn.com>
2019-02-14 15:48:41 -0800

    Refactor sprial order

--- spiral_matrix/spiral_matrix.js
+++ spiral_matrix/spiral_matrix.js
@@ -22,61 +22,38 @@ Input:
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 */
 const spiralOrder = (matrix) => {
-  // create container variables
-  // top = 0, left = 0
-  // bottom = matrix.length - 1, right = matrix[0].length - 1
-  let top = 1,
-    left = 0,
-    bottom = matrix.length - 1,
-    right = matrix[0].length - 1;
-  let r = 'r',
-    d = 'd',
-    l = 'l',
-    u = 'u';
-  let directions = [r, d, l, u];
-  let dir = 0;
-  let row = 0,
-    col = 0;
-  let total = matrix.length * matrix[0].length;
+  if (matrix.length === 0) return [];
+  let rowStart = 0,
+    rowEnd = matrix.length - 1,
+    colStart = 0,
+    colEnd = matrix[0].length - 1;
   let result = [];
-  while (result.length !== total) {
-    let el = matrix[row][col];
-    result.push(el);
-    let direction = directions[dir % 4];
-    if (direction === r) {
-      col++;
-      if (col === right) {
-        dir++;
-        right--;
-      }
-    } else if (direction === 'd') {
-      row++;
-      if (row === bottom) {
-        dir++;
-        bottom--;
-      }
-    } else if (direction === 'l') {
-      col--;
-      if (col === left) {
-        dir++;
-        left++;
-      }
-    } else {
-      row--;
-      if (row === top) {
-        dir++;
-        top++;
+  while (colEnd >= colStart && rowEnd >= rowStart) {
+    for (let col = colStart; col <= colEnd; col++) {
+      result.push(matrix[rowStart][col]);
+    }
+    rowStart++;
+
+    for (let row = rowStart; row <= rowEnd; row++) {
+      result.push(matrix[row][colEnd]);
+    }
+    colEnd--;
+
+    if (rowEnd >= rowStart) {
+      for (let col = colEnd; col >= colStart; col--) {
+        result.push(matrix[rowEnd][col]);
       }
     }
+    rowEnd--;
+
+    if (colEnd >= colStart) {
+      for (let row = rowEnd; row >= rowStart; row--) {
+        result.push(matrix[row][colStart]);
+      }
+    }
+    colStart++;
   }
   return result;
-  // start at top left
-  ///// increase col num until hitting right
-  ///// decrease right var
-  ///// increase row num until hitting bottom
-  ///// decrease bottom var
-  ///// decrease col until hitting left
-  ///// increase left
 };
 
 module.exports = spiralOrder;
