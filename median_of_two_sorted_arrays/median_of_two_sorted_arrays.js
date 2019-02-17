fd4dd7fec43d0b001fa2c0dcdd0339872b67953d
Steven Inouye <steveninouye@msn.com>
2019-02-16 23:26:18 -0800

    Complete median of two sorted arrays

--- median_of_two_sorted_arrays/median_of_two_sorted_arrays.js
+++ median_of_two_sorted_arrays/median_of_two_sorted_arrays.js
@@ -21,28 +21,71 @@ The median is (2 + 3)/2 = 2.5
 */
 
 const findMedianSortedArrays = (nums1, nums2) => {
-  // find the center index number O(1);
   let len1 = nums1.length;
   let len2 = nums2.length;
   let totalLength = len1 + len2;
   let [short, shortLen, long, longLen] =
     len1 < len2 ? [nums1, len1, nums2, len2] : [nums2, len2, nums1, len1];
-  if (short[shortLen - 1] < long[0] || short[0] > long[longLen - 1]) {
+  if (len1 === 0 || len2 === 0) {
+    let nums = short.concat(long);
+    let idx = findMidIdx(0, nums.length - 1);
+    if (nums.length % 2 === 1) {
+      return nums[idx];
+    } else {
+      return (nums[idx] + nums[idx + 1]) / 2;
+    }
+  } else if (short[shortLen - 1] <= long[0] || short[0] >= long[longLen - 1]) {
     let idx = Math.floor((totalLength - 1) / 2);
-    if (short[shortLen - 1] < long[0]) {
+    if (short[shortLen - 1] <= long[0]) {
       idx -= shortLen;
     }
     if (totalLength % 2 === 1) {
       return long[idx];
+    } else if (idx === -1) {
+      return (long[0] + short[shortLen - 1]) / 2;
+    } else if (idx === shortLen - 1) {
+      return (short[0] + long[longLen - 1]) / 2;
     } else {
       return (long[idx] + long[idx + 1]) / 2;
     }
   } else {
+    let start = 0;
+    let end = shortLen - 1;
+    let shortIdx = findMidIdx(start, end);
+    let longIdx = getLongIdx(totalLength, shortIdx);
+    let max = Math.max(
+      short[shortIdx] || -Infinity,
+      long[longIdx] || -Infinity
+    );
+    let min = Math.min(
+      short[shortIdx + 1] || Infinity,
+      long[longIdx + 1] || Infinity
+    );
+    while (max > min) {
+      if (short[shortIdx] > long[longIdx + 1]) {
+        end = shortIdx - 1;
+      } else {
+        start = shortIdx + 1;
+      }
+      shortIdx = findMidIdx(start, end);
+      longIdx = getLongIdx(totalLength, shortIdx);
+      max = Math.max(short[shortIdx] || -Infinity, long[longIdx] || -Infinity);
+      min = Math.min(
+        short[shortIdx + 1] || Infinity,
+        long[longIdx + 1] || Infinity
+      );
+    }
+    if (totalLength % 2 === 1) {
+      return max;
+    } else {
+      return (max + min) / 2;
+    }
   }
-  return true;
 };
 
-const getLongTake = (totalLength, shortTakeLeft) =>
-  Math.ceil(totalLength / 2) - shortTakeLeft;
+const findMidIdx = (start, end) => Math.floor((end - start) / 2) + start;
+
+const getLongIdx = (totalLength, shortIdx) =>
+  Math.ceil(totalLength / 2) - shortIdx - 2;
 
 module.exports = findMedianSortedArrays;
--- median_of_two_sorted_arrays/median_of_two_sorted_arrays.test.js
+++ median_of_two_sorted_arrays/median_of_two_sorted_arrays.test.js
@@ -6,19 +6,52 @@ describe('#findMedianSortedArrays', () => {
     const arr2 = [1];
     expect(findMedianSortedArrays(arr1, arr2)).toBe(4);
   });
+
+  it('should return the element in the middle of an odd amount of elements', () => {
+    const arr1 = [4, 5];
+    const arr2 = [4];
+    expect(findMedianSortedArrays(arr1, arr2)).toBe(4);
+  });
+
   it('should return the average of the 2 middle elements if there are an even number of elements', () => {
     const arr1 = [1, 2, 3];
     const arr2 = [4, 5, 6, 7, 8];
     expect(findMedianSortedArrays(arr1, arr2)).toBe(4.5);
   });
+
+  it('should return the average of the 2 middle elements if there are an even number of elements', () => {
+    const arr1 = [1, 2, 3];
+    const arr2 = [3, 5, 6, 7, 8];
+    expect(findMedianSortedArrays(arr1, arr2)).toBe(4);
+  });
+
   it('should return the element in the middle of an odd amount of elements', () => {
     const arr1 = [4, 5];
-    const arr2 = [7];
+    const arr2 = [5];
     expect(findMedianSortedArrays(arr1, arr2)).toBe(5);
   });
+
   it('should return the average of the 2 middle elements if there are an even number of elements', () => {
     const arr1 = [1, 2, 3, 4, 5, 6];
     const arr2 = [7, 8];
     expect(findMedianSortedArrays(arr1, arr2)).toBe(4.5);
+  });
+
+  it('should return the average of the 2 middle elements if there are an even number of elements', () => {
+    const arr1 = [1, 2, 3, 4];
+    const arr2 = [2, 4, 5, 7];
+    expect(findMedianSortedArrays(arr1, arr2)).toBe(3.5);
+  });
+
+  it('should return the average of the 2 middle elements if there are an even number of elements', () => {
+    const arr1 = [1, 2];
+    const arr2 = [3, 4];
+    expect(findMedianSortedArrays(arr1, arr2)).toBe(2.5);
+  });
+
+  it('should return the average of the 2 middle elements if there are an even number of elements', () => {
+    const arr1 = [2];
+    const arr2 = [1, 3, 4];
+    expect(findMedianSortedArrays(arr1, arr2)).toBe(2.5);
   });
 });
