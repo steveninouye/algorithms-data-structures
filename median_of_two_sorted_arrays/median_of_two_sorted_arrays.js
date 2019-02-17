79958e46cab3a4bfd1a0e58a4307452f28f82fb4
Steven Inouye <steveninouye@msn.com>
2019-02-16 20:18:36 -0800

    Find Initial middle points

--- median_of_two_sorted_arrays/median_of_two_sorted_arrays.js
+++ median_of_two_sorted_arrays/median_of_two_sorted_arrays.js
@@ -22,6 +22,20 @@ The median is (2 + 3)/2 = 2.5
 
 const findMedianSortedArrays = (nums1, nums2) => {
   // find the center index number O(1);
+  let len1 = nums1.length;
+  let len2 = nums2.length;
+  let isOdd = (len1 + len2) % 2 === 1;
+  let [short, shortLen, long, longLen] =
+    len1 < len2 ? [nums1, len1, nums2, len2] : [nums2, len2, nums1, len1];
+  let shortMidPt = Math.floor(shortLen - 1 / 2);
+  let longMidPt = Math.ceil((len1 + len2)/2) - shortMidPt - 2;
+  let longMaxLeft = long[longMidPt ] || -Infinity
+  let shortMaxLeft = short[shortMidPt] || -Infinity
+  let longMinRight = long[longMidPt + 1] || Infinity
+  let shortMinRight = short[shortMidPt + 1] || Infinity
+  while(Math.max(longMaxLeft,shortMaxLeft) <= Math.min(longMinRight,shortMinRight)){
+    
+  }
 };
 
 module.exports = findMedianSortedArrays;
