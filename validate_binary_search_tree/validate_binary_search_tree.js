85ff8c9c001736b8987527b35b3ea10e8a9cfd8b
Steven Inouye <steveninouye@msn.com>
2019-02-16 18:09:45 -0800

    Complete validate vinary search tree

--- validate_binary_search_tree/validate_binary_search_tree.js
+++ validate_binary_search_tree/validate_binary_search_tree.js
@@ -1,3 +1,20 @@
-const validateBinarySearchTree = (root) => {};
+const validateBinarySearchTree = (root) => {
+  return validateBTree(root, -Infinity, Infinity);
+};
+
+const validateBTree = (root, min, max) => {
+  if (!root) {
+    return true;
+  }
+
+  if (root.val <= min || root.val >= max) {
+    return false;
+  }
+
+  return (
+    validateBTree(root.left, min, root.val) &&
+    validateBTree(root.right, root.val, max)
+  );
+};
 
 module.exports = validateBinarySearchTree;
