09b2dc3b2a2bb16029c508bbe9b57d9417791f34
Steven Inouye <steveninouye@msn.com>
2019-02-16 17:03:13 -0800

    Create files for validate binary search tree

---
+++ validate_binary_search_tree/validate_binary_search_tree.test.js
@@ -0,0 +1,7 @@
+const validateBinarySearchTree = require('./validate_binary_search_tree');
+
+describe('#validateBinarySearchTree', () => {
+  it('should return true if tree is valid', () => {
+    
+  })
+})
