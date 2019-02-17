c55912baee61e83a4498b344a971045d169b3c93
Steven Inouye <steveninouye@msn.com>
2019-02-17 09:12:14 -0800

    Create tests for ListNode

--- merge_k_sorted_lists/merge_k_sorted_lists.test.js
+++ merge_k_sorted_lists/merge_k_sorted_lists.test.js
@@ -1,4 +1,4 @@
-const { ListNode, mergeKLists } = require('./merge_k_sorted_lists');
+const { MinHeap, ListNode, mergeKLists } = require('./merge_k_sorted_lists');
 
 const generateListofLinkLists = (list) => {
   return list.map((arr) => arrToLinkList(arr));
@@ -24,6 +24,58 @@ const linkListToArray = (root) => {
   }
   return result;
 };
+
+describe('ListNode', () => {
+  let root, child1, child2, child3;
+
+  it('should be defined', () => {
+    expect(ListNode).toBeDefined();
+  });
+
+  beforeEach(() => {
+    root = new ListNode(1);
+    child1 = new ListNode(2);
+    child2 = new ListNode(3);
+    child3 = new ListNode(4);
+  });
+
+  it('should be able to link 2 nodes together', () => {
+    expect(root.val).toBe(1);
+    expect(child1.val).toBe(2);
+    root.next = child1;
+    expect(root.next.val).toBe(2);
+  });
+
+  it('should be able to link multiple nodes togeter', () => {
+    child2.next = child3;
+    child1.next = child2;
+    root.next = child1;
+    expect(root.val).toBe(1);
+    expect(root.next.val).toBe(2);
+    expect(root.next.next.val).toBe(3);
+    expect(root.next.next.next.val).toBe(4);
+  });
+});
+
+describe('MinHeap', () => {
+  it('should be defined', () => {
+    expect(MinHeap).toBeDefined()
+  })
+
+  it('should have a store',() => {
+
+  })
+
+  it('should have push function on its prototype', () => {
+
+  })
+
+  it('should have extract function on its prototype', () => {
+
+  })
+
+  
+})
 
 describe('#mergeKLists', () => {
   it('sorts lists', () => {
