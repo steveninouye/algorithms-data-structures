8bcb8e48bf4d06373949972ad1a312b8381ad3da
Steven Inouye <steveninouye@msn.com>
2019-02-18 20:17:48 -0800

    Change trees to typescript files

--- data_structures/trees/trees.ts
+++ data_structures/trees/trees.ts
@@ -1,6 +1,9 @@
 import { Node, SinglyLinkNode, DoublyLinkNode } from '../nodes/nodes';
 
 export class BinaryTreeNode extends Node {
+  left: BinaryTreeNode;
+  right: BinaryTreeNode;
+
   constructor(val) {
     super(val);
     this.left = null;
