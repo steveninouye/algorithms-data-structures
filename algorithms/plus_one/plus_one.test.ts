09d2d814c23e55400ed4e16afc726f8c59f319bb
Steven Inouye <steveninouye@msn.com>
2019-02-20 22:36:06 -0800

    Write tests for plus one

--- algorithms/plus_one/plus_one.test.ts
+++ algorithms/plus_one/plus_one.test.ts
@@ -4,4 +4,14 @@ describe('plus one', () => {
   it('should be defined', () => {
     expect(plusOne).toBeDefined();
   });
+
+  it('should add one to the last element in the array if element is less than 9', () => {
+    expect(plusOne([1, 2, 3])).toEqual([1, 2, 4]);
+    expect(plusOne([4, 3, 2, 1])).toEqual([4, 3, 2, 2]);
+  });
+
+  it('should change the last element to zero and add 1 to the previous element if previous element is less than 9', () => {
+    expect(plusOne([1, 2, 9])).toEqual([1, 3, 0]);
+    expect(plusOne([4, 3, 2, 9])).toEqual([4, 3, 3, 0]);
+  });
 });
