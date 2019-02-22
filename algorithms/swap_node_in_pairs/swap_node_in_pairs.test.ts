0160dd10848373c67c3e0de1bc2e0efd61774bc0
Steven Inouye <steveninouye@msn.com>
2019-02-21 22:44:14 -0800

    import singly link node into test

--- algorithms/swap_node_in_pairs/swap_node_in_pairs.test.ts
+++ algorithms/swap_node_in_pairs/swap_node_in_pairs.test.ts
@@ -1,9 +1,8 @@
-import swapPairs from './swap_node_in_pairs'
+import { SinglyLinkNode } from '../../data_structures/nodes/nodes';
+import swapPairs from './swap_node_in_pairs';
 
 describe('swap pairs', () => {
   it('should be defined', () => {
     expect(swapPairs).toBeDefined();
-  })
-
-  
-})
+  });
+});
