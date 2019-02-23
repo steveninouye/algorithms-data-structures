de078ac39b069478f613ee1f79310f99c303e678
Steven Inouye <steveninouye@msn.com>
2019-02-22 20:07:30 -0800

    Add graphs quiz to project

---
+++ data_structures/graphs/graphs_skeleton/lib/graph.rb
@@ -0,0 +1,19 @@
+class Vertex
+
+  def initialize(value)
+  end
+end
+
+class Edge
+
+  def initialize(from_vertex, to_vertex, cost = 1)
+    
+  end
+
+  def destroy!
+    
+  end
+
+  protected
+  
+end
