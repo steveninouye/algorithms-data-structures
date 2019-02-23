7ab39c537fa39fb887e1bfd1df1201a89c6ea783
Steven Inouye <steveninouye@msn.com>
2019-02-22 21:23:18 -0800

    Start Kahn's algorithm

--- data_structures/graphs/graphs_skeleton/lib/topological_sort.rb
+++ data_structures/graphs/graphs_skeleton/lib/topological_sort.rb
@@ -1,17 +1,28 @@
-require_relative 'graph'
+require_relative "graph"
 
 # Kahn's
 # O(|V| + |E|).
 def topological_sort(vertices)
+  sorted_vertices = []
+  queue = []
+  edges = {}
+
+  vertices.each do |vertex|
+    in_edges_cost = vertex.in_edges.reduce(0) { |acc, edge| acc + edge.cost }
+    if in_edges_cost === 0
+      queue << vertex
+    else
+      edges[vertex] = in_edges_cost
+    end
+  end
+
   
 end
 
 # Tarjans
 
 def topological_sort(vertices)
-  
 end
 
 def dfs!(vertex, explored, ordering)
-  
 end
