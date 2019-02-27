09ec88ac0df9fe59242b986596b86c6dc52dc858
Steven Inouye <steveninouye@msn.com>
2019-02-26 20:56:20 -0800

    Change add two numbers to typescript

---
+++ algorithms/add_two_numbers/add_two_number.ts
@@ -0,0 +1,28 @@
+import { SinglyLinkNode as ListNode } from '../../data_structures/nodes/nodes';
+/*
+https://leetcode.com/problems/add-two-numbers/
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Definition for singly-linked list.
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+*/
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = (l1, l2) => {};
+
+export default addTwoNumbers;
