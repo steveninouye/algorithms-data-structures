c2d817226eb187c3415d1b59814fc3dc83d52e94
Steven Inouye <steveninouye@msn.com>
2019-02-27 11:14:44 -0800

    Write inital test for length of longest substring

---
+++ algorithms/longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.test.ts
@@ -0,0 +1,7 @@
+import lengthOfLongestSubstring from './longest_substring_without_repeating_characters';
+
+describe('#lengthOfLongestSubstring', () => {
+  it('should be defined', () => {
+    expect(lengthOfLongestSubstring).toBeDefined();
+  });
+});
