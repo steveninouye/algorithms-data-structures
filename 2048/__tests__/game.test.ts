5a80e409a4a3ab2052e4db73347e8952f9a8ef19
Steven Inouye <steveninouye@msn.com>
2019-02-28 08:53:53 -0800

    write intial game test statements

---
+++ 2048/__tests__/game.test.ts
@@ -0,0 +1,25 @@
+import Game from "../game";
+
+describe('Game', () => {
+  it('should be defined', () => {
+
+  })
+  
+  it('should initialize with new board', () => {
+
+  })
+
+  describe("#gameOver", () => {
+    it('should be true when board is full AND no pieces can move', () => {
+
+    })
+
+    it('should not be true when pieces can move', () => {
+
+    })
+
+    it("when spaces are available on the board", () => {
+      
+    })
+  })
+})
