c7c786a50c27ed6ce24d9bae019f5a27aa081f43
Steven Inouye <steveninouye@msn.com>
2019-03-05 16:15:37 -0800

    complete four sum

--- algorithms/4sum/4Sum.ts
+++ algorithms/4sum/4Sum.ts
@@ -20,8 +20,29 @@ A solution set is:
 
 export const fourSum = (nums: number[], target: number): number[][] => {
   const result:number[][] = []
-  if(nums.length < 4) return result
+  const len = nums.length
+  if(len < 4) return result
   nums.sort((a,b) => a - b)
-  
+  for(var i = 0; i < len - 3; i++) {
+    if(i !== 0 && nums[i] === nums[i - 1]) continue;
+    const num1 = nums[i]
+    for(var j = i + 1; j < len -2; j++) {
+      const num2 = nums[j]
+      if(num1 + (num2 *3) > 0) break;
+      let lo = j + 1
+      let hi = len - 1
+      while(lo < hi) {
+        const sum = nums[lo] + nums[hi]
+        const total = num1 + num2 + sum
+        if(total === 0){
+          result.push([num1, num2, nums[lo], nums[hi]])
+        } else if (total > 0){
+          hi--
+        } else {
+          lo++
+        }
+      }
+    }
+  }
   return result;
 };
