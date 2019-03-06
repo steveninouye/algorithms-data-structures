8da396d434dd3f030d78696b96b71694a259fe3b
Steven Inouye <steveninouye@msn.com>
2019-03-05 19:00:58 -0800

    Complete divide tow integers

--- algorithms/divide_two_integers/divide_two_integers.ts
+++ algorithms/divide_two_integers/divide_two_integers.ts
@@ -20,4 +20,21 @@ Both dividend and divisor will be 32-bit signed integers.
 The divisor will never be 0.
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
 */
-export const divide = (dividend: number, divisor: number): number => {};
+export const divide = (dividend: number, divisor: number): number => {
+  let result = 0;
+  if (Math.abs(divisor) > Math.abs(dividend)) return result;
+  if ((dividend >= 0 && divisor > 0) || (dividend <= 0 && divisor < 0)) {
+    while (Math.abs(dividend) >= Math.abs(divisor)) {
+      result += 1;
+      dividend -= divisor;
+    }
+  } else {
+    while (Math.abs(dividend) >= Math.abs(divisor)) {
+      result -= 1;
+      dividend += divisor;
+    }
+  }
+  if (result > 2147483647) return 2147483647;
+  if (result < -2147483648) return -2147483648;
+  return result;
+};
