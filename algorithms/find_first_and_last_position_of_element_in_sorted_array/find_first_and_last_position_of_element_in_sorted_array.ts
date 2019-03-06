363305ff6027d5aa9eec9589b43ce7d925dc7696
Steven Inouye <steveninouye@msn.com>
2019-03-05 19:59:19 -0800

    fix bug in search range

--- algorithms/find_first_and_last_position_of_element_in_sorted_array/find_first_and_last_position_of_element_in_sorted_array.ts
+++ algorithms/find_first_and_last_position_of_element_in_sorted_array/find_first_and_last_position_of_element_in_sorted_array.ts
@@ -24,17 +24,17 @@ const searchRange = (nums: number[], target: number): number[] => {
   if (num === target) {
     let left = searchRange(nums.slice(0, midPt), target);
     let right = searchRange(nums.slice(midPt + 1), target);
-    if(right[0] === -1 && left[0] === -1){
-      return [-1, -1]
+    if (right[0] === -1 && left[0] === -1) {
+      return [midPt, midPt];
     } else if (right[0] === -1 && left[0] > -1) {
-      left[1] = midPt
+      left[1] = midPt;
       return left;
     } else if (left[0] === -1 && right[0] > -1) {
-      right[0] += midPt + 1
-      right[1] += midPt + 1
+      right[0] += midPt + 1;
+      right[1] += midPt + 1;
       return right;
     } else {
-      left[1] = midPt + 1 + right[1]
+      left[1] = midPt + 1 + right[1];
       return left;
     }
   } else if (num < target) {
@@ -42,8 +42,8 @@ const searchRange = (nums: number[], target: number): number[] => {
     if (right[0] === -1) {
       return right;
     } else {
-      right[0] += midPt + 1
-      right[1] += midPt + 1
+      right[0] += midPt + 1;
+      right[1] += midPt + 1;
       return right;
     }
   } else {
