7b21ecd7006951b95d76371656578454d33ba4ce
Steven Inouye <steveninouye@msn.com>
2019-03-06 15:35:43 -0800

    Clear logest palindrom

--- algorithms/longest_palindrome/longest_palindromic_substring.ts
+++ algorithms/longest_palindrome/longest_palindromic_substring.ts
@@ -19,19 +19,7 @@ Output: "bb"
  * @param {string} str
  * @returns {string}
  */
-export const longestPalindrome = (str: string): string => {
-  if (str.length === 0) return '';
-  let len = str.length;
-  while (len > 1) {
-    for (let i = 0; i + len <= str.length; i++) {
-      if (isPalindrome(str, i, len - 1)) {
-        return str.slice(i, i + len);
-      }
-    }
-    len--;
-  }
-  return str[0];
-};
+export const longestPalindrome = (str: string): string => {};
 
 /**
  *
@@ -39,16 +27,4 @@ export const longestPalindrome = (str: string): string => {
  * @param {string} str
  * @returns {boolean}
  */
-export const isPalindrome = (
-  str: string,
-  start: number,
-  end: number
-): boolean => {
-  const midPt = Math.floor((end - start + 1) / 2);
-  for (let i = 0; i < midPt; i++) {
-    if (str[i] !== str[end - i]) {
-      return false;
-    }
-  }
-  return true;
-};
+export const isPalindrome = (): boolean => {};
