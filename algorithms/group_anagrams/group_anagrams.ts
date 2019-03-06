bc516e8e6699dc318f316639fbdb0457b7a68c81
Steven Inouye <steveninouye@msn.com>
2019-03-06 11:44:55 -0800

    Complete group anagrams

--- algorithms/group_anagrams/group_anagrams.ts
+++ algorithms/group_anagrams/group_anagrams.ts
@@ -18,7 +18,22 @@ The order of your output does not matter.
 */
 
 const groupAnagrams = (strs: string[]): string[][] => {
-  return [['ate', 'eat', 'tea'], ['nat', 'tan'], ['bat']];
+  const store = {};
+  for (var word of strs) {
+    const key = word
+      .split('')
+      .sort()
+      .join('');
+    if (!store[key]) {
+      store[key] = [];
+    }
+    store[key].push(word);
+  }
+  const result = [];
+  for (var words in store) {
+    result.push(store[words]);
+  }
+  return result;
 };
 
 console.log(
