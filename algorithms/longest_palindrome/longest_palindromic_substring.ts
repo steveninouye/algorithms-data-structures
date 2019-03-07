e1c98bf44ea835357c31790573b1733883c5cac1
Steven Inouye <steveninouye@msn.com>
2019-03-07 09:21:06 -0800

    Complete longest Palindrome

--- algorithms/longest_palindrome/longest_palindromic_substring.ts
+++ algorithms/longest_palindrome/longest_palindromic_substring.ts
@@ -19,7 +19,28 @@ Output: "bb"
  * @param {string} str
  * @returns {string}
  */
-export const longestPalindrome = (str: string): string => {};
+export const longestPalindrome = (str: string): string => {
+  /*
+  starting with the length of the entire string
+  starting with the index of 0
+  increment index by one until index plus length is greater than the length of the string
+  once the index plus the length is greater than the length of the string degrement the length and restart the index at 0
+  */
+  if (str.length === 0) return str;
+  let idx = 0;
+  let len = str.length;
+  while (len > 1) {
+    while (idx + len <= str.length) {
+      if (isPalindrome(str, idx, len - 1)) {
+        return str.slice(idx, len);
+      }
+      idx++;
+    }
+    idx = 0;
+    len--;
+  }
+  return str[0];
+};
 
 /**
  *
@@ -27,4 +48,15 @@ export const longestPalindrome = (str: string): string => {};
  * @param {string} str
  * @returns {boolean}
  */
-export const isPalindrome = (): boolean => {};
+export const isPalindrome = (
+  str: string,
+  start: number,
+  end: number
+): boolean => {
+  const midPt = Math.floor((start + end) / 2);
+  for (var i = start; i <= midPt; i++) {
+    const right = end - (i - start);
+    if (str[i] !== str[right]) return false;
+  }
+  return true;
+};
