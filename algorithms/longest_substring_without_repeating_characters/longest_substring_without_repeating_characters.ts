540cd1c4c9600b7e0c504b5fc1d42bc583d6b5bb
Steven Inouye <steveninouye@msn.com>
2019-03-07 10:10:10 -0800

    Complete length of longest substring

--- algorithms/longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.ts
+++ algorithms/longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.ts
@@ -21,22 +21,69 @@ Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-const lengthOfLongestSubstring = (str: string): number => {
-  if (str.length === 0) return 0;
-  let currIdx: number = 0;
-  let maxLength: number = 1;
-  let store = { [str[0]]: true };
-  for (let i = 1; i < str.length; i++) {
-    while (store[str[i]]) {
-      let ltr: string = str[currIdx];
-      delete store[ltr];
-      currIdx++;
-    }
-    store[str[i]] = true;
-    let currLength: number = i - currIdx + 1;
-    maxLength = maxLength < currLength ? currLength : maxLength;
+class Store {
+  store: {};
+  count: number;
+
+  constructor() {
+    this.store = {};
+    this.count = 0;
   }
-  return maxLength;
+
+  has(letter: string) {
+    return !!this.store[letter];
+  }
+
+  add(letter: string) {
+    this.store[letter] = true;
+    this.count++;
+  }
+
+  delete(letter: string) {
+    delete this.store[letter];
+    this.count--;
+  }
+}
+
+const lengthOfLongestSubstring = (str: string): number => {
+  /*
+   * create variable with the value of zero
+   *   this variable will hold the max length  which we will return
+   * create a store which will be a Set/Hash that will keep track of seen letters
+   * create slow pointer with index of zero wich will examine the index in the string
+   * create a faster pointer with index of one wich will examine the index of the string
+   *
+   * iterate UNTIL slow pointer + max is greater than str length
+   *   examine if fast pointer is in the store
+   *   if in store
+   *     move slow pointer up while deleting letter from store UNTIL deleting letter of fast pointer
+   *   add fast pointer letter
+   *   If count in store is greater than max value assign it to the new variable
+   *
+   * return max value
+   */
+
+  // might need to handle edge case of string being empty
+  let max = 0;
+  const store = new Store();
+  let slowIdx = 0;
+  let fastIdx = 0;
+  while (fastIdx < str.length && slowIdx + max < str.length) {
+    const fastLtr = str[fastIdx];
+    if (store.has(fastLtr)) {
+      while (str[slowIdx] !== fastLtr) {
+        const slowLtr = str[slowIdx];
+        store.delete(slowLtr);
+        slowIdx++;
+      }
+      slowIdx++;
+    } else {
+      store.add(fastLtr);
+      max = Math.max(max, store.count);
+    }
+    fastIdx++;
+  }
+  return max;
 };
 
 export default lengthOfLongestSubstring;
