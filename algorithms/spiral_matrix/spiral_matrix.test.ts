b81b5de2da2ba735591cd2596f10422904172422
Steven Inouye <steveninouye@msn.com>
2019-03-08 14:36:08 -0800

    Change spiral order to typescript

--- algorithms/spiral_matrix/spiral_matrix.test.ts
+++ algorithms/spiral_matrix/spiral_matrix.test.ts
@@ -1,4 +1,4 @@
-const spiralOrder = require('./spiral_matrix');
+import { spiralOrder } from './spiral_matrix';
 
 test('[  [ 1, 2, 3 ],  [ 4, 5, 6 ],  [ 7, 8, 9 ] ] should equal [1,2,3,6,9,8,7,4,5]', () => {
   expect(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([
