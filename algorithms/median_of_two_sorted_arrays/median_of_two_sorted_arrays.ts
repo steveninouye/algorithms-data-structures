570f34583279b2a25572bb6191f90375f5106078
Steven Inouye <steveninouye@msn.com>
2019-03-07 17:18:11 -0800

    Configure median of 2 sorted arrays

--- algorithms/median_of_two_sorted_arrays/median_of_two_sorted_arrays.ts
+++ algorithms/median_of_two_sorted_arrays/median_of_two_sorted_arrays.ts
@@ -22,10 +22,73 @@ The median is (2 + 3)/2 = 2.5
 
 export const findMedianSortedArrays = (nums1: number[], nums2: number[]) => {
   /*
+   * constraints:
    * make sure that the length of left(nums1) + left(nums2) === right(nums1) + right(nums2)
    * possible difference of 1
+   * MAX of BOTH left arrays needs to be less than MIN of both right arrays
+   * find the middle number
+   * if there are 2 middle numbers (length is even) then average of the 2 middle numbers
+   *
+   * Let's start with doing a binary search on the short array to find out where to cut
+   *
    */
+  const [short, shortLen, long, longLen] =
+    nums1.length > nums2.length
+      ? [nums2, nums2.length, nums1, nums1.length]
+      : [nums1, nums1.length, nums2, nums2.length];
+  if (shortLen === 0) {
+    return arrayMedian(long);
+  } else if (short[0] > long[longLen - 1] || long[0] > short[shortLen - 1]) {
+    
+  } else {
+    let combinedMidPt = getLeftMidPt(shortLen + longLen);
+    let shortMidPt = getLeftMidPt(shortLen);
+    let longMidPt = combinedMidPt - shortMidPt - 1;
+    let leftMax = Math.max(short[shortMidPt], long[longMidPt]);
+    let rightMin = Math.min(short[shortMidPt + 1], long[longMidPt + 1]);
+    while (leftMax > rightMin) {
+      if (short[shortMidPt] === leftMax) {
+        shortMidPt = getLeftMidPt(shortMidPt + 1);
+        longMidPt = combinedMidPt - shortMidPt - 1;
+      } else {
+        const midPtShift = getLeftMidPt(shortLen - shortMidPt - 1) + 1;
+        shortMidPt += midPtShift;
+        longMidPt = combinedMidPt - shortMidPt - 1;
+      }
+      leftMax = Math.max(
+        short[shortMidPt] || -Infinity,
+        long[longMidPt] || -Infinity
+      );
+      rightMin = Math.min(
+        short[shortMidPt + 1] || Infinity,
+        long[longMidPt + 1] || Infinity
+      );
+    }
+    if (combinedMidPt % 2 === 1) {
+      return Math.min(short[shortMidPt + 1], long[longMidPt + 1]);
+    } else {
+      let max = Math.max(short[shortMidPt], long[longMidPt]);
+      let min = Math.min(short[shortMidPt + 1], long[longMidPt + 1]);
+      return (max + min) / 2;
+    }
+  }
 };
+
+// get median of array
+const arrayMedian = (arr: number[]): number => {
+  let idx = getLeftMidPt(arr.length);
+  if (arr.length % 2 === 1) {
+    return arr[idx];
+  } else {
+    return (arr[idx] + arr[idx + 1]) / 2;
+  }
+};
+
+// mid point with left bias
+const getLeftMidPt = (length: number): number => Math.floor((length - 1) / 2);
+
+// mid point with right bias
+const getRightMidPt = (length: number): number => Math.floor(length / 2);
 
 // const findMidIdx = (start, end) => Math.floor((end - start) / 2) + start;
 
