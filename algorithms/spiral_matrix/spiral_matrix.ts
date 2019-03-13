d0861dcfe8a883125c4e7c110f2cb7d3cfe148f6
Steven Inouye <steveninouye@msn.com>
2019-03-12 19:02:54 -0700

    Reset spiral matrix problem

--- algorithms/spiral_matrix/spiral_matrix.ts
+++ algorithms/spiral_matrix/spiral_matrix.ts
@@ -22,34 +22,4 @@ Input:
 ]
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 */
-export const spiralOrder = (matrix: number[][]): number[] => {
-  const unWrappedMatrix: number[] = [];
-  let topRow = 0;
-  let bottomRow = matrix.length - 1;
-  let leftCol = 0;
-  let rightCol = matrix[0].length - 1;
-  while (topRow <= bottomRow && leftCol <= rightCol) {
-    for (let col = leftCol; col <= rightCol; col++) {
-      unWrappedMatrix.push(matrix[topRow][col]);
-    }
-    topRow++;
-    for (let row = topRow; row <= bottomRow; row++) {
-      unWrappedMatrix.push(matrix[row][rightCol]);
-    }
-    rightCol--;
-    if (topRow <= bottomRow) {
-      for (let col = rightCol; col >= leftCol; col--) {
-        unWrappedMatrix.push(matrix[bottomRow][col]);
-      }
-      bottomRow--;
-    }
-    if (leftCol <= rightCol) {
-      for (let row = bottomRow; row >= topRow; row--) {
-        unWrappedMatrix.push(matrix[row][leftCol]);
-      }
-      leftCol++;
-    }
-  }
-
-  return unWrappedMatrix;
-};
+export const spiralOrder = (matrix: number[][]): number[] => {};
