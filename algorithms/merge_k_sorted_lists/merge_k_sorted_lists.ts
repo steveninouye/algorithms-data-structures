cf9919c8d2504a582fba01b0936b2a38883530a4
Steven Inouye <steveninouye@msn.com>
2019-03-25 19:48:54 -0700

    Complete main structure of merge k sorted lists

--- algorithms/merge_k_sorted_lists/merge_k_sorted_lists.ts
+++ algorithms/merge_k_sorted_lists/merge_k_sorted_lists.ts
@@ -1,67 +1,28 @@
-// class ListNode {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
-// }
+class ListNode {
+  val: any;
+  next: ListNode;
 
-// class MinHeap {
-//   constructor() {
-//     this.store = [];
-//   }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 
-//   extract() {
-//     let result = this.store[0];
-//     let val = this.store.pop();
-//     this.store[0] = val;
-//     let childIndexs = this.childIdxs(0);
-//     while (childIndexs.length > 0) {
-//       let validIdxs = childIndexs.filter((i) => {
-//         return this.store[i] < val;
-//       });
-//       if (validIdxs.length === 0) {
-//         break;
-//       } else if (validIdxs.length === 1) {
-//       } else {
-//       }
-//     }
-//     return result;
-//   }
-
-//   push(val) {
-//     this.store.push(val);
-//     let childIndex = this.store.length - 1;
-//     let parentIndex = this.parentIdx(childIndex);
-//     while (parentIndex) {
-//       let parent = this.store[parentIndex];
-//       let child = this.store[childIndex];
-//       if (child < parent) {
-//         let temp = child;
-//         child = parent;
-//         parent = temp;
-//         childIndex = parentIndex;
-//         parentIndex = this.parentIdx(childIndex);
-//       } else {
-//         parentIndex = null;
-//       }
-//     }
-//     return this;
-//   }
-
-//   childIdxs(parentIdx) {
-//     let idx1 = parentIdx * 2 + 1;
-//     idx1 = idx1 < this.store.length ? idx1 : null;
-//     let idx2 = parentIdx * 2 + 2;
-//     idx2 = idx2 < this.store.length ? idx2 : null;
-//     return [idx1, idx2].filter((e) => e);
-//   }
-
-//   parentIdx(childIdx) {
-//     if (childIdx === 0) return null;
-//     return Math.floor((childIdx - 1) / 2);
-//   }
-// }
+  
+}
 
 export const mergeKLists = (list: ListNode[]): ListNode => {
-  
+  const head = new ListNode(0);
+  let currNode = head;
+  const heap = new MinHeap();
+  for (let node of list) {
+    heap.push(node);
+  }
+  while (heap.store.length > 0) {
+    const node = heap.extract();
+    currNode.next = node;
+    if (node.next) {
+      heap.push(node.next);
+    }
+  }
+  return head.next;
 };
