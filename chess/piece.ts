75bbe9251b8296dffccdda3b54c14ecc121091ee
Steven Inouye <steveninouye@msn.com>
2019-03-29 12:14:29 -0700

    Create constructor for Piece class in Chess

--- chess/piece.ts
+++ chess/piece.ts
@@ -1,7 +1,29 @@
+import { Board } from './board';
+
+type Pos = { row: number; col: number };
+
 export class Piece {
-  pos: { row: number; col: number };
-  constructor(row: number, col: number) {
-    this.pos = { row, col };
-  }
+  color: string;
+  board: Board;
+  pos: Pos;
+  validMoves: Pos[];
   
+  constructor(color: string, board: Board, row: number, col: number) {
+    this.color = color;
+    this.board = board;
+    this.pos = { row, col };
+    this.validMoves = [];
+  }
+
+  move() {}
+
+  getMoves() {
+    this.getAllMoves();
+    this.filterMoves();
+    return this.movesAvailable;
+  }
+
+  getAllMoves(pos: Pos) {}
+
+  filterMoves() {}
 }
